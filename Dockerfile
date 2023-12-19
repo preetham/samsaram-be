e65ed8b4c3beffbb376b44f04be8924f91e87779
Preetham <kamidipreetham@gmail.com>
2023-12-19 08:13:41 +0530

    Add: Basic files
    Signed-off-by: Preetham <kamidipreetham@gmail.com>

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:3.10.9-slim-bullseye
-COPY . /app/samsaram
-WORKDIR /app/samsaram
-RUN pip install -r requirements.txt
-EXPOSE 8080
-ENTRYPOINT ["python", "wsgi.py"]
--- util/log.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logger = logging.getLogger()
-
-_handler = logging.StreamHandler()
-_handler.setLevel(logging.DEBUG)
-_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-
-logger.addHandler(_handler)
